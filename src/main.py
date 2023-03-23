1cb05052841294052cb4accd431e859954e63704
lnnam <lenam.fithcmus@gmail.com>
2023-03-23 14:40:45 +0700

    push

--- src/main.py
+++ src/main.py
@@ -1,38 +1,63 @@
+import argparse
+from random import random, randint
+from itertools import product, combinations
+
 from graph import *
+from algorithms import pa_bfs, pa_kl, pa_fm, pa_sb
 
-from algorithms import pa_bfs, pa_kl, pa_sb
-
-# main
-g = Graph()
-for i in range(6):
-    g.addVertex(i)
-g.addEdge(0, 1, 5)
-g.addEdge(0, 5, 2)
-g.addEdge(1, 2, 4)
-g.addEdge(2, 3, 9)
-g.addEdge(3, 4, 7)
-g.addEdge(3, 5, 3)
-g.addEdge(4, 0, 1)
-g.addEdge(5, 4, 8)
-g.addEdge(5, 2, 1)
+set_logger('logs/')
 
 
-# lst_vertices = g.vertList
+def generate_graph(n, p, directed=True,):
+    g = Graph()
+    for idx in range(n):
+        g.addVertex(idx)
+    possible_edges = product(
+        range(n), repeat=2) if directed else combinations(range(n), 2)
+    for u, v in possible_edges:
+        if random() < p:
+            weight = randint(1, 2*n)
+            g.addEdge(u, v, weight)
+            if not directed:
+                g.addEdge(v, u, weight)
+    return g
 
-pa_sb(g)
 
-# pa_kl(g)
-# g.compute_adjacency_matrix()
-# g.degree_nodes()
+if __name__ == '__main__':
+    # Configure logging
+    FORMAT = '%(asctime)s.%(msecs)03d %(message)s'
+    logging.basicConfig(format=FORMAT, datefmt='%H:%M:%S')
+    parser = argparse.ArgumentParser(
+        description="Compute the partition of a graph.")
 
-# for k, v in g.vertList.items():
-#     print(v)
-#     for key, item in v.connectedTo.items():
-#         print(v.getWeight(key))
+    # for generating graph randomly
+    parser.add_argument('--number_nodes', '-n',
+                        help='Total nodes of graph', type=int, default=10)
+    parser.add_argument(
+        '--edge_prob', '-p', help='Probability for choose edge', type=float, default=0.5)
+    parser.add_argument('--is_directed', '-d',
+                        help='Probability for choose edge', type=bool, default=False)
 
-#     print('----')
+    # for performing algorithms
+    parser.add_argument(
+        '--threshold', '-t', help='Threshold for BFS partitioning', type=int, default=3)
+    parser.add_argument(
+        '--algorithm', '-a', type=str, help='Algorithm for compute the partition of a graph.'
+        'bfs: Breadth First Search, kl: Kerninghan-Lin (KL) algorithm, fm: Fiduccia-Mattheyses (FM) algorithm, sb: Spectral Bisection', default='bfs')
 
-# print(L1)
-# print(L2)
+    args = parser.parse_args()
+    print(args)
 
-# print(g.weight_func(1, 0))
+    g = generate_graph(args.number_nodes, args.edge_prob, args.is_directed)
+
+    logging.info('Performing graph partition algorithm')
+    if args.algorithm == 'bfs':
+        group_a, group_b = pa_bfs(g, vertex_ith=0, threshold=args.threshold)
+    elif args.algorithm == 'kl':
+        cutset_size, group_a, group_b = pa_kl(g)
+    elif args.algorithm == 'fm':
+        cutset_size, group_a, group_b = pa_fm(g)
+    elif args.algorithm == 'sb':
+        group_a, group_b = pa_sb(g)
+    else:
+        logging.warning('Algorithm not supported')
